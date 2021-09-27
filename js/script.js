72ad419fad65c08a4e74dc0e8af820bc7d924f3d
caju <35105475+elcaju@users.noreply.github.com>
2021-09-27 19:08:45 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
 var url = window.location.search;
 var params = new URLSearchParams(url);
 var id = params.get('cranium_id')
-console.log(id);
+// console.log(id);
 var staticUrl = 'https://raw.githubusercontent.com/recklesslabs/wickedcraniums/main/' + id;
 
+
+// Render the Cranium
 
 async function getCranium() {
     const response = await fetch(staticUrl);
     const data = await response.json();
 
     document.getElementById('cranium').src = data.image
-    console.log(data.image);
+    // console.log(data.image);
 
     
 }
 
 getCranium();
 
-// ------------------------------
+// Get the dropdown value
+
+function changeFunction(selectValue) {
+    var clothing = selectValue.value;
+    document.getElementById('clothes').src = "images/" + clothing + ".png";
+    // console.log(clothing)
+}
+
+// Render the canvas 
 
 $(document).ready(function () {
 
@@ -29,8 +39,8 @@ $(document).ready(function () {
             }).then(function (canvas) {
                 var anchorTag = document.createElement("a");
                 document.body.appendChild(anchorTag);
-                document.getElementById("teste").appendChild(canvas);
-                anchorTag.download = "WickedSuit.jpg";
+                document.getElementById("final_image").appendChild(canvas);
+                anchorTag.download = "WickedClothing.jpg";
                 anchorTag.href = canvas.toDataURL();
                 anchorTag.target = '_blank';
                 anchorTag.click();
